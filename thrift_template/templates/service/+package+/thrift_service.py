f91004ec93404fd6bac3947ab7b5908d45a4bd5b
yancl <kmoving@gmail.com>
2013-08-15 14:29:06 +0800

    add service post prepare callback

--- thrift_template/templates/service/+package+/thrift_service.py
+++ thrift_template/templates/service/+package+/thrift_service.py
@@ -4,10 +4,6 @@ from thrift.server.TServer import TThreadPoolServer
 from thrift.server.TNonblockingServer import TNonblockingServer
 
 
-def post_prepare(self, port):
-    print 'running on port:',port
-
-
 def threadpool_prepare(self):
     """Start a fixed number of worker threads and put client into a queue"""
     for i in range(self.threads):
@@ -21,13 +17,8 @@ def threadpool_prepare(self):
     # Pump the socket for clients
     self.serverTransport.listen()
 
+
 def threadpool_serve(self):
-    self.prepare()
-
-    #do something after address binding
-    #for example, register self to nameservice
-    self.post_prepare(self.serverTransport.port)
-
     while True:
       try:
         client = self.serverTransport.accept()
@@ -42,44 +33,50 @@ def nonblock_serve(self):
     Serve requests forever, or until stop() is called.
     """
     self._stop = False
-    self.prepare()
-
-    self.post_prepare(self.socket.port)
-
     while not self._stop:
         self.handle()
 
-TThreadPoolServer.post_prepare = post_prepare
 TThreadPoolServer.prepare = threadpool_prepare
 TThreadPoolServer.serve =  threadpool_serve
-
-
-TNonblockingServer.post_prepare = post_prepare
 TNonblockingServer.serve =  nonblock_serve
 
+
+def post_prepare(app, global_conf, **kwargs):
+    print 'global_conf:', global_conf
+    print 'kwargs:', kwargs
+    if hasattr(app, 'post_prepare_callback'):
+        getattr(app, 'post_prepare_callback')(global_conf, **kwargs)
+
+
 def thread_pool_server_runner(app, global_conf, **kwargs):
-    for name in ['port', 'pool_size']:
-        if name in kwargs:
-            kwargs[name] = int(kwargs[name])
-    pool_size = kwargs.pop('pool_size')
-    host = kwargs.pop('host', '0.0.0.0')
-    transport = TSocket.TServerSocket(**kwargs)
-    transport.host = host
+    host = kwargs.get('host', '0.0.0.0')
+    port = int(kwargs.get('port', 9090))
+    pool_size = int(kwargs.get('pool_size', 10))
+    transport = TSocket.TServerSocket(host=host, port=port)
+    #transport.host = host
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
     server = TThreadPoolServer(app, transport, tfactory, pfactory)
     if pool_size:
         server.threads = pool_size
+
+    server.prepare()
+
+    post_prepare(app, global_conf, **kwargs)
+
     server.serve()
 
 
 def nonblock_server_runner(app, global_conf, **kwargs):
-    for name in ['port']:
-        if name in kwargs:
-            kwargs[name] = int(kwargs[name])
-    host = kwargs.pop('host', '0.0.0.0')
-    transport = TSocket.TServerSocket(**kwargs)
-    transport.host = host
+    host = kwargs.get('host', '0.0.0.0')
+    port = int(kwargs.get('port', 9090))
+    transport = TSocket.TServerSocket(host=host, port=port)
+    #transport.host = host
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
     server = TNonblockingServer(app, transport, pfactory, pfactory)
+
+    server.prepare()
+
+    post_prepare(app, global_conf, **kwargs)
+
     server.serve()
